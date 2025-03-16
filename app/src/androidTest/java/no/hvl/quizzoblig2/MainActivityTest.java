e5e036deb41dec27cf28433f07860ae587f589e0
h591443 <591443@stud.hvl.no>
2025-03-16 13:54:48 +0100

    Fikset tests

--- app/src/androidTest/java/no/hvl/quizzoblig2/MainActivityTest.java
+++ app/src/androidTest/java/no/hvl/quizzoblig2/MainActivityTest.java
@@ -1,36 +1,64 @@
 package no.hvl.quizzoblig2;
 
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-import no.hvl.quizzoblig2.R;
+import no.hvl.quizzoblig2.data.GalleryRepository;
+import no.hvl.quizzoblig2.data.db.GalleryItem;
 import no.hvl.quizzoblig2.ui.main.MainActivity;
 
-@RunWith(AndroidJUnit4.class)
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+
 public class MainActivityTest {
-
     @Rule
-    public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
+    public ActivityScenarioRule<MainActivity> activityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
 
-    @Test
-    public void testNavigateToGallery() {
-        onView(withId(R.id.btnGallery)).perform(click());
-        onView(withId(R.id.recyclerViewGallery)).check(matches(isDisplayed()));
+    @Before
+    public void setUp() {
+        // Add test images to ensure there are enough for quiz
+        activityScenarioRule.getScenario().onActivity(activity -> {
+            // Create a repository
+            GalleryRepository repository = new GalleryRepository(activity.getApplication());
+
+            // Add three test images from your resources
+            repository.insert(new GalleryItem("Dog", "android.resource://no.hvl.quizzoblig2/drawable/dog"));
+            repository.insert(new GalleryItem("Cat", "android.resource://no.hvl.quizzoblig2/drawable/cat"));
+            repository.insert(new GalleryItem("Fox", "android.resource://no.hvl.quizzoblig2/drawable/fox"));
+
+            // Give time for database operations to complete
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     @Test
-    public void testNavigateToQuiz() {
-        onView(withId(R.id.btnQuiz)).perform(click());
-        onView(withId(R.id.imageViewQuestion)).check(matches(isDisplayed()));
+    public void testNavigationToQuiz() {
+        Espresso.onView(ViewMatchers.withId(R.id.btnQuiz))
+                .perform(ViewActions.click());
+
+        // Sjekk at Quiz-fragmentet vises
+        Espresso.onView(ViewMatchers.withId(R.id.imageViewQuestion))
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testNavigationToGallery() {
+        Espresso.onView(ViewMatchers.withId(R.id.btnGallery))
+                .perform(ViewActions.click());
+
+        // Sjekk at Gallery-fragmentet vises
+        Espresso.onView(ViewMatchers.withId(R.id.recyclerViewGallery))
+                .check(matches(isDisplayed()));
     }
 }
